ccc5f726ee45c64527fda58512d6195aadd4db4b
joeljohnson <joeljohnson@attask.com>
2012-09-06 13:16:29 -0600

    Using a buffer rather than the list for the logs. Fixing the help page. Added (but commented out) caching the logs.

--- src/main/java/com/attask/jenkins/RunningFailReportAction.java
+++ src/main/java/com/attask/jenkins/RunningFailReportAction.java
@@ -4,29 +4,32 @@ import hudson.model.*;
 import hudson.tasks.junit.CaseResult;
 import hudson.tasks.junit.TestResultAction;
 
-import java.io.IOException;
+import java.io.*;
+import java.lang.ref.Reference;
+import java.lang.ref.SoftReference;
+import java.lang.ref.WeakReference;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 /**
- * Created with IntelliJ IDEA.
  * User: brianmondido
  * Date: 8/31/12
  * Time: 1:23 PM
- * To change this template use File | Settings | File Templates.
  */
-
 public class RunningFailReportAction implements Action {
-
     private static final int DEFAULT_AGE=1;
     private String buildId;
     private final String startPattern;
     private final int startGroupNumber;
     private final String endPattern;
     private final int endGroupNumber;
+
+//	private transient int lastLine = -1;
+//	private transient Reference<Map<String, String>> cachedMap;
 
     public RunningFailReportAction(String buildId, String startPattern, String startGroupNumber, String endPattern, String endGroupNumber){
         this.buildId=buildId;
@@ -37,40 +40,61 @@ public class RunningFailReportAction implements Action {
     }
 
     public Map<String, String> generateFailureReport() throws IOException {
-        Map<String, String> testOutput=new HashMap<String, String>();
         Run<?,?> run = AbstractBuild.fromExternalizableId(buildId);
 
-        List<String> log = run.getLog(Integer.MAX_VALUE);
-        StringBuilder stackTraceBuilder=new StringBuilder();
+        StringBuilder stackTraceBuilder = new StringBuilder();
 
         String testName = null;
         Pattern startPattern = Pattern.compile(this.startPattern);
         Pattern endPattern = Pattern.compile(this.endPattern);
-        for (String line : log) {
 
-            Matcher startMatcher = startPattern.matcher(line);
-            if(testName == null && startMatcher.find()) {
-                testName = startMatcher.group(startGroupNumber);
-                continue;
-            }
+		int count = 0;
+		Map<String, String> testOutput = null;
+//		if (cachedMap != null) {
+//			testOutput = cachedMap.get();
+//		}
+//		if(testOutput == null) {
+//			lastLine = -1;
+			testOutput = new ConcurrentHashMap<String, String>();
+//			cachedMap = new SoftReference<Map<String, String>>(testOutput);
+//		}
 
-            Matcher endMatcher = endPattern.matcher(line);
-            if(testName != null && endMatcher.find()){
-                String endOfTestName = endMatcher.group(endGroupNumber);
-                if(!endOfTestName.equals(testName)) {
-                    System.err.println("This is a weird state we're in! We started in one test and finished in another");
-                    continue;
-                }
-                testOutput.put(testName, stackTraceBuilder.toString());
-                stackTraceBuilder=new StringBuilder();
-                testName = null;
-                continue;
-            }
+		BufferedReader reader = new BufferedReader(new InputStreamReader(new FileInputStream(run.getLogFile()),run.getCharset()));
+		try {
+			for (String line = reader.readLine(); line != null; line = reader.readLine()) {
+				count++;
+//				if(lastLine >= 0 && count <= lastLine) {
+//					continue;
+//				}
 
-            if(testName != null){
-                stackTraceBuilder.append(line).append("\n");
-            }
-        }
+				Matcher startMatcher = startPattern.matcher(line);
+				if(testName == null && startMatcher.find()) {
+					testName = startMatcher.group(startGroupNumber);
+					continue;
+				}
+
+				Matcher endMatcher = endPattern.matcher(line);
+				if(testName != null && endMatcher.find()){
+					String endOfTestName = endMatcher.group(endGroupNumber);
+					if(!endOfTestName.equals(testName)) {
+						System.err.println("This is a weird state we're in! We started in one test and finished in another");
+						continue;
+					}
+					testOutput.put(testName, stackTraceBuilder.toString());
+					stackTraceBuilder=new StringBuilder();
+					testName = null;
+					continue;
+				}
+
+				if(testName != null){
+					stackTraceBuilder.append(line).append("\n");
+				}
+			}
+		} finally {
+//			lastLine = count;
+			reader.close();
+		}
+
         return testOutput;
     }
 
@@ -86,7 +110,7 @@ public class RunningFailReportAction implements Action {
             for (int i = 0; i < failedTests.size(); i++) {
                 CaseResult caseResult = failedTests.get(i);
                 String caseResultFullName = caseResult.getFullName();
-                if (caseResultFullName.equals(testName)) {
+                if (caseResultFullName.equals(testName.replace("#", "."))) {
                     age = caseResult.getAge() + DEFAULT_AGE;
                     i = failedTests.size();
                 }
@@ -130,11 +154,11 @@ public class RunningFailReportAction implements Action {
     }
 
     public String getIconFileName() {
-        return "/plugin/runningtestfailurereport/epic.jpg";
+        return "clipboard.png";
     }
 
     public String getDisplayName() {
-        return "Running Failure Report";
+        return "Test Result";
     }
 
     public String getUrlName() {
